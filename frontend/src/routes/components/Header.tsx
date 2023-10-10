d4c5ff11c221dbe7b034524d510bb30d415907c2
Dani-dornas <lauby.dd@gmail.com>
2023-10-09 21:10:19 -0300

    home page end

--- frontend/src/routes/components/Header.tsx
+++ frontend/src/routes/components/Header.tsx
@@ -3,32 +3,44 @@ import lupa from "./img/lupa.png";
 import logo from "./img/logo.png";
 import user from "./img/user.png";
 import config from "./img/config.png";
-import { Row, Col, Container } from "react-bootstrap";
-import "./Components.css"
+import { Row, Col, Container, AccordionHeader } from "react-bootstrap";
+import "./Components.css";
 
-function Header(){
-    return <header id="header">
-    <Container>
-      <Row>
-        <Col>
-          <Link className="logo" to="/"><img src={logo} alt="logo" /></Link>
-        </Col>
-        <Col>
-          <form action="" className="searchbar">
-            <input type="text" placeholder="Search..." />
-            <button type="submit"><img src={lupa} alt="search icon" /></button>
-          </form>
-        </Col>
-        <Col>
-          <nav id="header-nav">
-            <button className="botao-user"><img src={user} alt="Pagina de usuário" /></button>
-            <button className="botao-config"><img src={config} alt="Pagina de configuração" /></button>
-          </nav>
-        </Col>
-      </Row>
+function Header() {
+  const black = "black";
 
-    </Container>
-  </header>
+  return (
+    <header id="header">
+      <Container>
+        <Row>
+          <Col>
+            <Link className="logo" to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </Col>
+          <Col>
+            <form action="" className="searchbar">
+              <input type="text" placeholder="Search..." />
+              <button type="submit">
+                <img src={lupa} alt="search icon" />
+              </button>
+            </form>
+          </Col>
+          <Col>
+            <nav id="header-nav">
+              <button className="botao-user">
+                <img src={user} alt="Pagina de usuário" />
+              </button>
+
+              <button className="botao-config">
+                <img src={config} alt="Pagina de configuração" />
+              </button>
+            </nav>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
