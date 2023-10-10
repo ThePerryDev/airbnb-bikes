ce295394af46815427ed216b4beafc16a880db25
Willian-Garcia <williangarcia710@hotmail.com>
2023-10-09 22:45:11 -0300

    Novo layout

--- frontend/src/routes/pages/login/Login.tsx
+++ frontend/src/routes/pages/login/Login.tsx
@@ -1,28 +1,53 @@
 import { Link } from "react-router-dom";
-import "./login.css"
+import { Button, Col, Container, Row, Tab, Tabs } from "react-bootstrap";
+import "./login.css";
 
-function Login(){
-    return(
-        <div>
-            <header>
-              
-            </header>
-            <main>
-              <div className="cont">
-                <div className="form sign-in">
-                  <h2>Entrar</h2>
-                  <label>
-                    <button className="continuar" type="button">Continuar com Facebook</button>
-                  </label>
-                  <label>
-                    <button className="continuar" type="button">Continuar com Google</button>
-                  </label>
-                </div>
-              </div>
-            </main>
-            <Link to="/">Voltar</Link>
-        </div>
-    )
+function Login() {
+  return (
+    <div id="body">
+      <header></header>
+      <main>
+        <Container fluid id="fundo">
+          <Col
+            className="d-flex align-items-center justify-content-center"
+
+          >
+            <Tabs
+              defaultActiveKey="profile"
+              id="fill-tab-example"
+              className="d-flex align-items-center justify-content-center"
+            >
+              <Tab eventKey="entrar" title="Entrar" id="aba">
+                <Row id="linha">
+                  <Col md={12} className="d-flex align-items-center justify-content-center">
+                    <div>
+                      <Button id="botao">Continuar com Facebook</Button>
+                    </div>
+                    <div>
+                      <Button id="botao">Continuar com Facebook</Button>
+                    </div>
+                  </Col>
+                </Row>
+              </Tab>
+              <Tab eventKey="registrar" title="Registrar" id="aba">
+              <Row id="linha">
+                  <Col md={12} className="d-flex align-items-center justify-content-center">
+                    <div>
+                      <Button id="botao">Continuar com Facebook</Button>
+                    </div>
+                    <div>
+                      <Button id="botao">Continuar com Facebook</Button>
+                    </div>
+                  </Col>
+                </Row>
+              </Tab>
+            </Tabs>
+          </Col>
+        </Container>
+      </main>
+      <Link to="/">Voltar</Link>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
