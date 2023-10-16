2e337fc3cf3ccdefde764222149e1ad79db1eb33
Willian Garcia <williangarcia710@hotmail.com>
2023-10-16 00:07:48 -0300

    Configurando OAuth e primeiros testes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 import routes from "./routes";
 import dotenv from "dotenv";
 import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+const GoogleStrategy = require('passport-google-oauth20');
+
 dotenv.config();
 
 // será usado 3000 se a variável de ambiente não tiver sido definida
@@ -15,9 +19,49 @@ app.use(cors(corsOptions))
 // suportar parâmetros JSON no body da requisição
 app.use(express.json());
 // inicializa o servidor na porta especificada
+app.use(
+  session({
+    secret:"secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user:any, done:any) =>{
+  return done(null, user);
+});
+
+passport.deserializeUser((user:any, done:any) =>{
+  return done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: "483922791543-ee87iheehpji4eho814ers8p1givsb7r.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "/auth/google/callback"
+},
+function(accessToken:any, refreshToken:any, profile:any, cb:any) {
+  console.log(profile);
+  cb(null, profile);
+}));
+  
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  });
+
 app.listen(PORT, () => {
   console.log(`Rodando na porta ${PORT}`);
 });
+
 
 // define a rota para o pacote /routes
 app.use(routes);
