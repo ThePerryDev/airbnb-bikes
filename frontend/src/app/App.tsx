742f8aac6a646c8f7c4482cc9f042c1cce8f6fc8
Eduardo270704 <luis270704@gmail.com>
2023-10-20 19:47:11 -0300

    merge

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,6 +5,7 @@ import Bike from "../routes/pages/bikes/Bike";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Demo from "../routes/demo/Demo";
 import Home from "../routes/home/Home";
+import Catalog from "../routes/pages/catalogo/Catalog";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/bike" element={<Bike />} />
         <Route path="/bike/:id" element={<Bike />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
       </Routes>
     </BrowserRouter>
   );
