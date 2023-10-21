d44c72ffaa9c6309e1be83f0b7ced03751d783be
Eduardo Moraes <30940593+Eduardo270704@users.noreply.github.com>
2023-10-20 22:40:17 -0300

    arrumando problemas

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,8 +5,8 @@ import Bike from "../routes/pages/bikes/Bike";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Demo from "../routes/demo/Demo";
 import Home from "../routes/home/Home";
+import Catalog from "../routes/pages/catalogo/Catalog";
 
-//teste, tá sangrando
 
 function App() {
   return (
@@ -18,7 +18,8 @@ function App() {
         <Route path="/" element={<Demo />} />
         <Route path="/bike" element={<Bike />} />
         <Route path="/bike/:id" element={<Bike />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />}/>
+        <Route path="/catalog" element={<Catalog />} />
       </Routes>
     </BrowserRouter>
   );
