b4af6f9318d0d5a64e80dd6dfd72f1970c5d23bf
Eduardo Moraes <30940593+Eduardo270704@users.noreply.github.com>
2023-10-20 22:22:50 -0300

    Merge

--- frontend/src/routes/demo/Demo.tsx
+++ frontend/src/routes/demo/Demo.tsx
@@ -9,7 +9,7 @@ function Demo() {
     <div id="body">
       <div className="conteudo">
         <h1>Welcome to the Bikepass demo!</h1>
-        <nav id="nav">
+        <nav>
           <p>Please, select the page you want to see:</p>
           <select id="paginas" onChange={(e) => setNavegar(e.target.value)}>
             <option value="/" selected>
@@ -20,6 +20,7 @@ function Demo() {
             <option value="/user">User</option>
             <option value="/bike">Bike</option>
             <option value="/home">Home</option>
+            <option value="/catalog">Catalog</option>
           </select>
           <Link to={navegar}>Confirma</Link>
         </nav>
