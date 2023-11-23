4581373dc89bb5faac208823571e1519367d7276
Willian-Garcia <williangarcia710@hotmail.com>
2023-11-22 23:05:13 -0300

    Atualizando estilização da pagina de Login

--- frontend/src/routes/pages/Login/Login.tsx
+++ frontend/src/routes/pages/Login/Login.tsx
@@ -4,13 +4,54 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/auth/Authcontext";
 import "./Login.css";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 export const Login = () => {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
-    <div id="loginbox">
+    <div id="mainy">
+      <main id="principal">
+        <Container className="d-flex align-items-start justify-content-center">
+          <Row>
+            <Col
+              md={12}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <Card
+                className="d-flex align-items-center justify-content-center"
+                id="caixa"
+              >
+                <Card.Body>
+                  <Card.Title>Login</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Faça seu Login com o Google
+                  </Card.Subtitle>
+                  <Card.Text>
+                    <GoogleLogin
+                      onSuccess={(credentialResponse) => {
+                        const token: any = credentialResponse.credential;
+                        const decoded = jwtDecode<JwtPayload>(token);
+                        let email = "" + decoded.email;
+                        let alias = "" + decoded.name;
+                        let jtiToken = "" + decoded.jti;
+                        auth.signin(alias, email, jtiToken);
+                        navigate("/");
+                      }}
+                      onError={() => {
+                        console.log("login failed");
+                      }}
+                    />
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </div>
+    /*<div id="loginbox">
 
         <h1>Faça seu Login com o Google</h1>
 
@@ -29,6 +70,6 @@ export const Login = () => {
           }}
         />
 
-    </div>
+    </div>*/
   );
 };
