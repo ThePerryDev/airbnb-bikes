bd4e8882b2340363340e55c3fa2102d558d15dd8
Willian-Garcia <williangarcia710@hotmail.com>
2023-11-23 15:20:00 -0300

    Novo visual do Login

--- frontend/src/routes/pages/Login/Login.tsx
+++ frontend/src/routes/pages/Login/Login.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/auth/Authcontext";
 import "./Login.css";
 import { Card, Col, Container, Row } from "react-bootstrap";
+import bike_parking from "./img/bike-parking.png";
 
 export const Login = () => {
   const auth = useContext(AuthContext);
@@ -13,31 +14,27 @@ export const Login = () => {
   return (
     <div id="mainy">
       <main id="principal">
-        <Container className="d-flex align-items-start justify-content-center">
+        <Container className="d-flex align-items-center justify-content-center">
           <Row>
-            <Col
-              md={12}
-              className="d-flex align-items-center justify-content-center"
-            >
-              <Card
-                className="d-flex align-items-center justify-content-center"
-                id="caixa"
-              >
-                <Card.Body>
-                  <Card.Title
-                    id="titulo"
-                    className="d-flex justify-content-center"
-                  >
-                    <h2>Login</h2>
-                  </Card.Title>
-                  <Card.Subtitle
-                    id="subtitulo"
-                    className="mb-2 text-muted d-flex justify-content-center"
-                  >
-                    Utilize o Google para <br />Entrar ou se Registrar
-                  </Card.Subtitle>
-                  <Card.Text className="d-flex align-items-center justify-content-center" >
-                    <div id="botao">
+            <Card id="caixa">
+              <Col md={12}>
+                <Card.Body id="conteudo-caixa" className="d-flex align-items-center justify-content-center">
+                  <Col md={6} className="img-col">
+                    <img
+                      src={bike_parking}
+                      alt={bike_parking}
+                      id="imglogin"
+                      className="img-fluid"
+                    />
+                  </Col>
+                  <Col md={6} className="text-col d-flex flex-column justify-content-center align-items-center">
+                    <Card.Title id="titulo" className="mb-4 text-center">
+                      Login
+                    </Card.Title>
+                    <Card.Subtitle id="subtitulo" className="mb-4 text-muted text-center">
+                      Utilize o Google para <br/> Entrar ou se Registrar
+                    </Card.Subtitle>
+                    <Card.Text id="botao" className="text-center">
                       <GoogleLogin
                         onSuccess={(credentialResponse) => {
                           const token: any = credentialResponse.credential;
@@ -52,11 +49,11 @@ export const Login = () => {
                           console.log("login failed");
                         }}
                       />
-                    </div>
-                  </Card.Text>
+                    </Card.Text>
+                  </Col>
                 </Card.Body>
-              </Card>
-            </Col>
+              </Col>
+            </Card>
           </Row>
         </Container>
       </main>
